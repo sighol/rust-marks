8fa44514642a394acc99e67b9d57f030af5225f4
Sigurd Holsen <sighol@gmail.com>
2015-05-12 13:08:10 +0200

    Working Implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 extern crate rustc_serialize;
 extern crate getopts;
 
@@ -11,20 +12,16 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use std::collections::HashMap;
+use std::collections::BTreeMap;
 
-struct CommandlineOptions {
-	add_bookmark: bool,
-}
 
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} [options]", program);
     print!("{}", opts.usage(&brief));
 }
 
-fn read_json() -> HashMap<String,String> {
-    let path = Path::new(".bookmarks");
-
-    let mut file = match File::open(&path) {
+fn read_json(path: &Path) -> HashMap<String, String> {
+    let mut file = match File::open(path) {
     	Err(why) => panic!("Could not open {}: {}", path.display(), Error::description(&why)),
     	Ok(file) => file,
     };
@@ -35,12 +32,21 @@ fn read_json() -> HashMap<String,String> {
     	Ok(_) => 0,
     };
 
-    let mut map: HashMap<String,String> = json::decode(&s).unwrap();
+    let map: HashMap<String,String> = json::decode(&s).unwrap();
     map
 }
 
+fn write_json(path: &Path, map: &HashMap<String, String>) {
+	let output = json::encode(map).unwrap();
+
+	let mut f = File::create(path).unwrap();
+	f.write_all(output.as_bytes());
+	f.sync_all();
+}
+
 fn main() {
-	let map = read_json();
+	let path = Path::new(".bookmarks");
+    let mut map = read_json(&path);
 
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
@@ -48,13 +54,61 @@ fn main() {
     let cwd: String = cwd_path.to_str().unwrap().to_string();
 
     let mut opts = Options::new();
-    opts.optopt("a", "", "Add new bookmark", "NAME");
+    opts.optopt("a", "add", "Add new bookmark", "NAME");
+    opts.optopt("r", "remove", "Remove bookmark", "NAME");
+    opts.optflag("k", "keys", "List Keys");
 
-    let matches = opts.parse(&args[1..]).unwrap();
-
-    match matches.opt_str("a") {
-    	Some(a: String) => { map[a] = cwd },
-    	None => {},
+    let matches = match opts.parse(&args[1..]) {
+    	Ok(m) => m,
+    	Err(f) => {print_usage(&program, opts); panic!("{}", f)}
     };
 
+    let add = matches.opt_str("a");
+    let remove = matches.opt_str("r");
+
+    if add.is_some() {
+    	map.insert(add.unwrap(), cwd.clone());
+    	write_json(&path, &map);
+    }else if remove.is_some() {
+    	let bm = remove.unwrap();
+    	let bm_str = &bm;
+    	if map.contains_key(bm_str) {
+    		map.remove(bm_str);
+    		write_json(&path, &map);
+    	}
+    } else if matches.opt_present("k") {
+    	let keys = get_keys(&map);
+    	for key in keys {
+    		println!("{}", key);
+    	}
+    } else {
+   		print_map(&map);
+    }
+}
+
+fn get_keys(map: &HashMap<String, String>) -> Vec<&String> {
+	let mut keys: Vec<&String> = map.keys().collect();
+	keys.sort();
+	keys
+}
+
+fn print_map(map: &HashMap<String, String>) {
+	let mut max_len = 0;
+	for (key, value) in map {
+		if key.len() > max_len {
+			max_len = key.len();
+		}
+	}
+	max_len += 2;
+
+	let keys = get_keys(map);
+
+	for key in keys {
+		let mut bfr = "".to_string();
+		let len = key.len();
+		for i in 0..(max_len-len) {
+			bfr.insert(i, ' ');
+		}
+		println!("{}{} : {}", bfr, key, map.get(key).unwrap());
+	}
 }
