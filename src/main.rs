323664cc21f11e8e9744897cfa0bc5455492d5a0
Sigurd Holsen <sighol@gmail.com>
2015-05-13 12:01:45 +0200

    Reorder methods and use if let

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,60 @@ use std::path::{Path, PathBuf};
 use std::collections::HashMap;
 use std::collections::BTreeMap;
 
+fn main() {
+    let path_sting = get_path();
+    let path = Path::new(&path_sting);
+    let mut map = read_json(&path);
+
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let cwd_path = env::current_dir().unwrap();
+    let cwd: String = cwd_path.to_str().unwrap().to_string();
+
+    let mut opts = Options::new();
+    opts.optopt("a", "add", "Add new bookmark", "NAME");
+    opts.optopt("r", "remove", "Remove bookmark", "NAME");
+    opts.optflag("k", "keys", "List Keys");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => {
+            print_usage(&program, opts);
+            panic!("{}", f)
+        }
+    };
+
+    if let Some(a) = matches.opt_str("a") {
+        map.insert(a, cwd.clone());
+        write_json(&path, &map);
+    } else if let Some(bm) = matches.opt_str("r") {
+        let bm_str = &bm;
+        if map.contains_key(bm_str) {
+            map.remove(bm_str);
+            write_json(&path, &map);
+        }
+    } else if matches.opt_present("k") {
+        let keys = get_keys(&map);
+        for key in keys {
+            println!("{}", key);
+        }
+    } else if !matches.free.is_empty() {
+        let key = matches.free[0].clone();
+        if map.contains_key(&key) {
+            println!("{}", map.get(&key).unwrap());
+        } else {
+            panic!("Key not found: {}", key);
+        }
+    } else {
+        print_map(&map);
+    }
+}
+
+fn get_path() -> PathBuf {
+    let mut bookmark = env::home_dir().unwrap();
+    bookmark.push(".bookmarks");
+    bookmark
+}
 
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} [options]", program);
@@ -42,77 +96,6 @@ fn write_json(path: &Path, map: &HashMap<String, String>) {
 	let mut f = File::create(path).unwrap();
 	f.write_all(output.as_bytes());
 	f.sync_all();
-}
-
-fn get_path() -> PathBuf {
-	let mut bookmark = env::home_dir().unwrap();
-	bookmark.push(".bookmarks");
-	bookmark
-}
-
-fn main() {
-	let path_sting = get_path();
-	let path = Path::new(&path_sting);
-    let mut map = read_json(&path);
-
-    let args: Vec<String> = env::args().collect();
-    let program = args[0].clone();
-    let cwd_path = env::current_dir().unwrap();
-    let cwd: String = cwd_path.to_str().unwrap().to_string();
-
-    let mut opts = Options::new();
-    opts.optopt("a", "add", "Add new bookmark", "NAME");
-    opts.optopt("r", "remove", "Remove bookmark", "NAME");
-    opts.optflag("k", "keys", "List Keys");
-
-    let matches = match opts.parse(&args[1..]) {
-    	Ok(m) => m,
-    	Err(f) => {print_usage(&program, opts); panic!("{}", f)}
-    };
-
-    let mut num_matches = 0;
-
-    match matches.opt_str("a") {
-        Some(a) => {
-            map.insert(a, cwd.clone());
-            write_json(&path, &map);
-            num_matches += 1;
-        },
-        _ => {}
-    };
-
-    match matches.opt_str("r") {
-        Some(bm) => {
-            let bm_str = &bm;
-            if map.contains_key(bm_str) {
-                map.remove(bm_str);
-                write_json(&path, &map);
-            }
-            num_matches += 1;
-        },
-        _ => {}
-    };
-
-    if matches.opt_present("k") {
-        let keys = get_keys(&map);
-        for key in keys {
-            println!("{}", key);
-        }
-        num_matches += 1;
-    }
-
-    if !matches.free.is_empty() {
-        let key = matches.free[0].clone();
-        if map.contains_key(&key) {
-            println!("{}", map.get(&key).unwrap());
-        } else {
-            panic!("Key not found: {}", key);
-        }
-    }
-
-    if num_matches == 0 {
-        print_map(&map);
-    }
 }
 
 fn get_keys(map: &HashMap<String, String>) -> Vec<&String> {
